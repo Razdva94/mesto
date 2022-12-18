16f10bd57676e6c788c282c5e09d6b42f3666466
Razdva94 <115868609+Razdva94@users.noreply.github.com>
2022-12-18 12:45:32 +0300

    реализовано открытие попап

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,26 @@
-let main = document.querySelector('.main');
-let editButton = main.querySelector('.profile__button-container');
-let popup = main.querySelector('.popup');
-let crossButton = popup.querySelector('.popup__close-icon');
-let saveButton = popup.querySelector('.popup__button');
-let formElement = popup.querySelector('.popup__form');
+const main = document.querySelector('.main');
+const editButton = main.querySelector('.profile__button-container');
+const popup = main.querySelector('.popup');
+const crossButton = popup.querySelector('.popup__close-icon');
+const formElement = popup.querySelector('.popup__form');
+const nameInput = formElement.querySelector('.popup__input_type_name');
+const jobInput = formElement.querySelector('.popup__input_type_job');
+const nameOutput = main.querySelector('.profile__title');
+const jobOutput = main.querySelector('.profile__text');
 
 function handleFormSubmit (evt) {
   evt.preventDefault();
-
-let nameInput = formElement.querySelector('.popup__input_type_name');
-let jobInput = formElement.querySelector('.popup__input_type_job');
-let nameOutput = main.querySelector('.profile__title');
-let jobOutput = main.querySelector('.profile__text');
-
 jobOutput.textContent = jobInput.value;
 nameOutput.textContent = nameInput.value;
+popup.classList.remove('popup_opened');
 }
-
-
-
 
 formElement.addEventListener('submit', handleFormSubmit);
 
-
-
-
-function popupOpenClose(){
-  popup.classList.toggle('popup_opened');
+function popupOpen(){
+  popup.classList.add('popup_opened')
+  nameInput.textContent = nameOutput.value;
+  jobInput.textContent = jobOutput.value;
 }
-editButton.addEventListener('click', popupOpenClose);
-crossButton.addEventListener('click', popupOpenClose);
-saveButton.addEventListener('click',popupOpenClose );
+editButton.addEventListener('click', popupOpen);
 
- 
